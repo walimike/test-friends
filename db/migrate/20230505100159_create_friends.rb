cbab39d2eea4c1fa4343752ac4a4749d77c8f074
walimike <walimike139@gmail.com>
2023-05-05 13:13:46 +0300

    initial setup

---
+++ db/migrate/20230505100159_create_friends.rb
@@ -0,0 +1,11 @@
+class CreateFriends < ActiveRecord::Migration[7.0]
+  def change
+    create_table :friends do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :telephone
+
+      t.timestamps
+    end
+  end
+end
