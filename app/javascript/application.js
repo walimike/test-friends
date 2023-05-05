30f3fada3a75d25b55facb9143bb68d9fb7d913b
walimike <walimike139@gmail.com>
2023-05-05 15:26:06 +0300

    add v1

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,16 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+
+import { createApp } from "vue";
+import HelloWorld from "../components/HelloWorld.vue";
+
+const app = createApp({
+  components: {
+    HelloWorld,
+  },
+});
+
+app.mount("#app");
+
